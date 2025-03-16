f720aa49aa5d193dcaac2203ce9364c165cdb9a7
ongsttt52 <ongsttt52@gmail.com>
2025-03-16 16:30:56 +0900

    config : 스프링 부트 초기 환경 설정

---
+++ miniMall_BE/src/main/java/com/tomjerry/miniMall/MiniMallApplication.java
@@ -0,0 +1,13 @@
+package com.tomjerry.miniMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMallApplication.class, args);
+	}
+
+}
---
+++ miniMall_BE/src/test/java/com/tomjerry/miniMall/MiniMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomjerry.miniMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
